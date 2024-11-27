208217cdb647c9d33e674a16352da5c00d427b3c
V-Paritosh <paritoshv08@gmail.com>
2024-11-27 15:02:48 -0600

    Made the nav and footer their own files and load.js to add them back

---
+++ load.js
@@ -0,0 +1,20 @@
+function load() {
+  const x = new XMLHttpRequest();
+  x.open("GET", "footer.html", true);
+  x.onload = function () {
+    if (x.status === 200) {
+      document.getElementById("footer").innerHTML = x.responseText;
+    }
+  };
+  x.send();
+  
+  const y = new XMLHttpRequest();
+  y.open("GET", "navbar.html", true);
+  y.onload = function () {
+    if (y.status === 200) {
+      document.getElementById("navbar").innerHTML = y.responseText;
+    }
+  };
+  y.send();
+}
+window.onload = load;
